1ae10d45b6b73ba1b9a34c6829475b3cfa829858
Zahid Shaikh <Zahid.Shaikh@teknopoint.local>
2024-05-27 19:17:30 +0530

    Footer Api Rendering Done

---
+++ scripts/Header-Footer-Js/script.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    // Footer Api Call Funtion
+    function footer_apicall(method, url) {
+
+        var xhr = new XMLHttpRequest();
+
+        xhr.open(method, url);
+
+        xhr.onreadystatechange = function () {
+
+            if (this.readyState == 4 && this.status == 200) {
+                footer_render(this.responseText);
+            } else if (this.status == 404) {
+                console.log(`File not Found`)
+            }
+        }
+
+        xhr.send();
+    }
+
+    footer_apicall('GET', 'https://publish-p133703-e1305981.adobeaemcloud.com/content/experience-fragments/piramalfinance/in/en/site/footer/master.html');
+    // Footer Api Call Funtion
+
+    // Footer Render Function
+    function footer_render(footer_data) {
+        var flag = 0;
+        document.addEventListener('scroll', function () {
+            if (flag == 0) {
+                flag = 1;
+                document.querySelector('.footer').innerHTML = footer_data
+            }
+        })
+    }
+    // Footer Render Function
+})
