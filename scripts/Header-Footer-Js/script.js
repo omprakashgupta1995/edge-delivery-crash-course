1defe51df17a66f2aa2371e90c161bcb92663fde
Zahid Shaikh <Zahid.Shaikh@teknopoint.local>
2024-06-07 19:00:45 +0530

    Piramal footer work done

--- scripts/Header-Footer-Js/script.js
+++ scripts/Header-Footer-Js/script.js
@@ -1,38 +1,38 @@
-document.addEventListener('DOMContentLoaded', function () {
+// document.addEventListener('DOMContentLoaded', function () {
 
-    // Footer Api Call Funtion
-    function footer_apicall(method, url) {
+//     // Footer Api Call Funtion
+//     function footer_apicall(method, url) {
 
-        var xhr = new XMLHttpRequest();
+//         var xhr = new XMLHttpRequest();
 
-        xhr.open(method, url);
+//         xhr.open(method, url);
 
-        xhr.onreadystatechange = function () {
+//         xhr.onreadystatechange = function () {
 
-            if (this.readyState == 4 && this.status == 200) {
-                footer_render(this.responseText);
-            } else if (this.status == 404) {
-                console.log(`File not Found`)
-            }
-        }
+//             if (this.readyState == 4 && this.status == 200) {
+//                 footer_render(this.responseText);
+//             } else if (this.status == 404) {
+//                 console.log(`File not Found`)
+//             }
+//         }
 
-        xhr.send();
-    }
+//         xhr.send();
+//     }
 
-    footer_apicall('GET', 'https://publish-p133703-e1305981.adobeaemcloud.com/content/experience-fragments/piramalfinance/in/en/site/footer/master.html');
-    // Footer Api Call Funtion
+//     footer_apicall('GET', 'https://publish-p133703-e1305981.adobeaemcloud.com/content/experience-fragments/piramalfinance/in/en/site/footer/master.html');
+//     // Footer Api Call Funtion
 
-    // Footer Render Function
-    function footer_render(footer_data) {
-        var flag = 0;
-        footer_data = footer_data.replace(/\/etc.clientlibs\/piramalfinance/g, "https://publish-p133703-e1305981.adobeaemcloud.com/etc.clientlibs/piramalfinance");
-        footer_data = footer_data.replace(/\/content\/dam\/piramalfinance/g, "https://publish-p133703-e1305981.adobeaemcloud.com/content/dam/piramalfinance");
-        document.addEventListener('scroll', function () {
-            if (flag == 0) {
-                flag = 1;
-                document.querySelector('.footer').innerHTML = footer_data
-            }
-        })
-    }
-    // Footer Render Function
-})
+//     // Footer Render Function
+//     function footer_render(footer_data) {
+//         var flag = 0;
+//         footer_data = footer_data.replace(/\/etc.clientlibs\/piramalfinance/g, "https://publish-p133703-e1305981.adobeaemcloud.com/etc.clientlibs/piramalfinance");
+//         footer_data = footer_data.replace(/\/content\/dam\/piramalfinance/g, "https://publish-p133703-e1305981.adobeaemcloud.com/content/dam/piramalfinance");
+//         document.addEventListener('scroll', function () {
+//             if (flag == 0) {
+//                 flag = 1;
+//                 document.querySelector('.footer').innerHTML = footer_data
+//             }
+//         })
+//     }
+//     // Footer Render Function
+// })
